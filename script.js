6042adc9d711201821eb546e77a84eae0d19bae1
Alex Zhou <zhoua2002@gmail.com>
2025-04-15 23:54:02 -0400

    Dark Mode Button

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+  }
